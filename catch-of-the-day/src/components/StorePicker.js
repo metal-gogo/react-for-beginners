0e0e806b5e74650145c97ac3ad5d361efe4f7927
metal-gogo <daniel@beek.io>
2018-04-03 17:18:06 -0500

    ⭐️ creating our application layout with components

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,17 +1,17 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
 class StorePicker extends React.Component {
   render() {
     return (
       <Fragment>
-        { /* This is a comment in JSX */}
+        {/* This is a comment in JSX */}
         <form action="" className="store-selector">
           <h2>Please Enter a Store</h2>
-          <input type="text" name="" id="" required placeholder="Store Name"/>
+          <input type="text" name="" id="" required placeholder="Store Name" />
           <button type="submit">Visit store -> </button>
         </form>
       </Fragment>
-    )
+    );
   }
 }
 
