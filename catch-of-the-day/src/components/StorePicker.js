1a852922360b1b0d221cf4e40413c869ab17d1fb
metal-gogo <daniel@beek.io>
2018-04-04 10:14:21 -0500

    ⭐️ helper and utility functions

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,16 +1,19 @@
-import React, { Fragment } from "react";
+import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
   render() {
     return (
-      <Fragment>
-        {/* This is a comment in JSX */}
-        <form action="" className="store-selector">
-          <h2>Please Enter a Store</h2>
-          <input type="text" name="" id="" required placeholder="Store Name" />
-          <button type="submit">Visit store -> </button>
-        </form>
-      </Fragment>
+      <form action="" className="store-selector">
+        <h2>Please Enter a Store</h2>
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit store -> </button>
+      </form>
     );
   }
 }
