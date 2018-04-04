c58de011522fa31c7dcfba378c82773bfbea2c93
metal-gogo <daniel@beek.io>
2018-04-04 09:49:19 -0500

    ⭐️ stateless functional components

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,23 +1,40 @@
 import React from "react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="top">
-        <h1>
-          Catch
-          <span className="ofThe">
-            <span className="of">of</span>
-            <span className="the">the</span>
-          </span>
-          day
-        </h1>
-        <h3 className="tagline">
-          <span>{this.props.tagline}</span>
-        </h3>
-      </header>
-    );
-  }
-}
+/* Stateless functional components */
+const Header = ({ tagline }) => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">of</span>
+        <span className="the">the</span>
+      </span>
+      day
+    </h1>
+    <h3 className="tagline">
+      <span>{tagline}</span>
+    </h3>
+  </header>
+);
+
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header className="top">
+//         <h1>
+//           Catch
+//           <span className="ofThe">
+//             <span className="of">of</span>
+//             <span className="the">the</span>
+//           </span>
+//           day
+//         </h1>
+//         <h3 className="tagline">
+//           <span>{this.props.tagline}</span>
+//         </h3>
+//       </header>
+//     );
+//   }
+// }
 
 export default Header;
