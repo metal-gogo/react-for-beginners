ab577c7191e502da4fdb6d979a3608c93c890ca3
metal-gogo <daniel@beek.io>
2018-04-17 17:43:51 -0500

    ⭐️ updating our Order State

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -3,7 +3,13 @@ import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
   render() {
-    const { desc, image, name, price, status } = this.props.details;
+    const {
+      addToOrder,
+      details: { desc, image, name, price, status },
+      index
+    } = this.props;
+    const isAvailable = status === "available";
+
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
@@ -12,7 +18,9 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add to Cart</button>
+        <button disabled={!isAvailable} onClick={() => addToOrder(index)}>
+          {isAvailable ? "Add to Cart" : "Sold Out!"}
+        </button>
       </li>
     );
   }
