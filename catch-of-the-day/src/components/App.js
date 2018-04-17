7f28299514c8c57d0f8046c7ec49bbff4f1972b7
metal-gogo <daniel@beek.io>
2018-04-17 17:25:49 -0500

    ⭐️ displaying state with JSX

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Fish from "./Fish";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
@@ -26,16 +27,24 @@ class App extends React.Component {
     this.setState({
       fishes: sampleFishes
     });
-  }
+  };
 
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} details={this.state.fishes[key]} />
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
